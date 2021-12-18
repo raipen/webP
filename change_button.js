7ca0b228d796567ae762e5a8b942284f6b59e26c
raipen <raipen@knu.ac.kr>
2021-12-18 22:44:31 +0900

    add star

--- change_button.js
+++ change_button.js
@@ -7,6 +7,7 @@ var curMain = 0;
 var nextMain = ["linear-gradient(to bottom, #008000, #004100)",
   "linear-gradient(to bottom, #000, #000010,rgba(0,0,50,1),rgba(0,0,70,1))",
   "linear-gradient(to right, #be4825 50%, #cf6f28 50%) 100%/30px 100%","#fff"];
+  
 function changeMain(){
   $('.main').addClass("disable");
   isloop[curMain] = false;
--- page2.js
+++ page2.js
@@ -1,20 +1,49 @@
 function init_main2(){
-
+  $('#main2').on("click",function(){
+    remove_santa();
+    add_santa();
+  });
 }
 
 function loop_main2(){
   if(isloop[2]){
     setTimeout(function(){
       twinkle_star();
+      delete_star(20,50);
       loop_main2();
     },random(50,300));
   }
 }
 
+function remove_santa(){
+
+}
+
+function add_santa(){
+
+}
+
+
+var color = ['skyblue','blue','yellow','purple'];
+
 function twinkle_star(){
   console.log("tw");
+  size = random(0,20)/100;
+  ran_col = random(0,3);
   x = random(0,window.innerWidth);
   y = random(0,window.innerHeight);
-  $('#main2').append('<span class="ball" style="top: '+y+'px;left:'+x+'px;background-color: #fff;transform: scale(1)"></span>');
-  $('.ball').last().css({"width": "100px","height":"100px"});
+  $('#main2').append('<span class="star" style="top: '+y+'px;left:'+x+'px;"></span>');
+  $('.star').last().css({"width": (340*size)+"px","height":(400*size)+"px"});
+  $('.star').last().addClass('shadow_'+color[ran_col]);
+}
+
+function delete_star(min_cnt,max_cnt){
+  if(random(min_cnt,max_cnt)<$('.star').length){
+    console.log("drop");
+    var eqeq=random(0,$('.star').length-1);
+    $('.star').eq(eqeq).css({"width":"scale(0)"});
+    setTimeout(function(){
+      $('.star').eq(eqeq).remove();
+    },45);
+  }
 }
