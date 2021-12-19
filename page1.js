122bc6ae60c7cccf83f1a47172b611a0fa775101
raipen <raipen@knu.ac.kr>
2021-12-19 15:59:09 +0900

    add tree deco type

--- page1.js
+++ page1.js
@@ -39,7 +39,6 @@ function loop_main1(){
 
 function make_background(){
   for(var i=5;i>0;i--){
-    console.log("sadf");
     $('#main1').append('<div class="tree_back" style="background-image:url(images/트리'+i+'.png); top:'+(25*(i==1?i-1:i-1.5))+'%"></div>');
   }
   $('.tree_back').last().css({"height":"25%"});
@@ -50,19 +49,47 @@ function add_item(){
   var ran = random(50,120);
   x = event.pageX-ran/2;
   y = event.pageY-ran/2;
-  $('#main1').append('<span class="ball" style="top: '+y+'px;left:'+x+'px;background-color: '+colorstring+';transform: scale(0)"></span>');
-  $('.ball').last().css({"width": ran,"height":ran});
-  setTimeout(function(){$('.ball').last().css({"transform": "scale(1)"});},30);
-  setTimeout(function(){$('.ball').last().addClass('swing');},500);
+  var item_type = ["cane","snow","ball","ball","ball","ball","ball","ball"];
+  var ran_type = random(0,item_type.length-1);
+  var item_html = '<span class="deco '+item_type[ran_type]
+  +'" style="top:'+y+'px;'
+  +'left:'+x+'px;';
+  if(ran_type>1){
+    item_html+='background-color: '+colorstring+';';
+    var ball_type = ["star","stripe","snow2","dot","none","none"];
+    var ball_size = [[10,20],[10,30],[20,40],[5,10],[0,0]];
+    var ran_ball = random(0,ball_type.length-1);
+    if(ran_ball==1){
+      color1 = 'rgb('+random(0,50)+','+random(150,250)+','+random(0,50)+')';
+      color2 = colorstring;
+      per = random(30,50);
+      item_html+='background-image: linear-gradient(to right,'+color1+' '+per+'%, '+color2+' '+per+'%);';
+      item_html+='background-size:'+random(ball_size[ran_ball][0],ball_size[ran_ball][1])+'px 100%;';
+    }
+    else if(ran_ball<4){
+      item_html+='background-image: url(images/'+ball_type[ran_ball]+'.png);';
+      item_html+='background-size: '+random(ball_size[ran_ball][0],ball_size[ran_ball][1])+'px;';
+    }
+    item_html+='width:'+ran+'px; height:'+ran+'px;';
+  }
+  else{
+    var rate = [572/350,529/484];
+    item_html+='width:'+ran+'px; height:'+ran*rate[ran_type]+'px;';
+  }
+  item_html +='transform: scale(0)"></span>';
+  $('#main1').append(item_html);
+  setTimeout(function(){$('.deco').last().css({"transform": "scale(1)"});},30);
+  setTimeout(function(){$('.deco').last().addClass('swing');},500);
 }
 
+
 function drop_item(min_cnt=1){
-  if(random(min_cnt,10)<$('.ball').length){
+  if(random(min_cnt,15)<$('.deco').length){
     console.log("drop");
-    var eqeq=random(0,$('.ball').length-min_cnt);
-    $('.ball').eq(eqeq).css({"transition":"1s ease-out","top": "calc(100% + 100px)"});
+    var eqeq=random(0,$('.deco').length-min_cnt);
+    $('.deco').eq(eqeq).css({"transition":"1s ease-out","top": "calc(100% + 100px)"});
     setTimeout(function(){
-      $('.ball').eq(eqeq).remove();
+      $('.deco').eq(eqeq).remove();
     },1000);
   }
 }
